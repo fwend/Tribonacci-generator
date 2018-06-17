9fc55787af27ba12f441c21a62b56c8e3a967868
Jos <josvanuden@gmail.com>
2018-06-17 14:35:29 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+function tribonacci(signature, n) {
+    const gen = tribonacciGen(signature);
+    for (let i = 0; i < n - 3; i++)
+        signature.push(gen.next().value);
+    return signature;
+}
+
+function* tribonacciGen(signature) {
+    let [a, b, c] = signature;
+    while (true) {
+        [a, b, c] = [b, c, a + b + c];
+        yield c;
+    }        
+}
